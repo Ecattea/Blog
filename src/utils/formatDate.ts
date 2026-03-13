8e458c064bf758b9e1f771fed8b862ebf7dde05c
Ecattea <147585044+Ecattea@users.noreply.github.com>
2026-03-13 23:17:43 +0800

    fixes ECA-74: polish article metadata cleanup
    - extract the shared display date format constant
    - precompute updated date and tag link view data
    - switch article metadata markup to semantic dl/dt/dd
    - tighten metadata CSS resets without changing behavior

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,6 @@
 import dayjs from "dayjs";
 
+export const DISPLAY_DATE_FORMAT = "YYYY:MM:DD";
+
 export const formatDisplayDate = (date: Date) =>
-  dayjs(date).format("YYYY:MM:DD");
+  dayjs(date).format(DISPLAY_DATE_FORMAT);
